d1a15ef29e85a3015b74e39d342b9c26f971d365
BaniHillabi <ibanihillabi2007@gmail.com>
2024-02-21 14:10:05 +0700

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,48 @@
-import React, { Component } from 'react';
-import './App.css';
-import Alert from './Components/Alert';
-// import element Alert yang telah dibuat  
+import React, { Component } from "react";
+import "./App.css";
+import Alert from "./Components/Alert";
+// import element Alert yang telah dibuat
 
 class App extends Component {
   render() {
     return (
       <>
-      <div className="App container col-sm-6">
-        <Alert type={this.state.type} header={this.state.header}>
-          {this.state.content}
-        </Alert>
-        <hr />
-        <h4>Alert Control</h4>
-        <b className="text-left">Tipe Alert</b>
-        <select className="form-control" name="type" value={this.state.type}
-          onChange={this.changeTypeAlert}>
-          <option value="success">Success</option>
-          <option value="warning">Warning</option>
-          <option value="danger">Danger</option>
-          <option value="info">Info</option>
-        </select>
-        <b className="text-left">Header Alert</b>
-        <input
-          type="text" name="header" className="form-control" value={this.state.header}
-          onChange={this.changeHeaderAlert} />
-        <b className="text-left">Content Alert</b>
-        <input
-          type="text" name="content" className="form-control" value={this.state.content}
-          onChange={this.changeContentAlert} />
-      </div>
-      <div></div>
+        <div className="App container col-sm-6">
+          <Alert type={this.state.type} header={this.state.header}>
+            {this.state.content}
+          </Alert>
+          <hr />
+          <h4>Alert Control</h4>
+          <b className="text-left">Tipe Alert</b>
+          <select
+            className="form-control"
+            name="type"
+            value={this.state.type}
+            onChange={this.changeTypeAlert}
+          >
+            <option value="success">Success</option>
+            <option value="warning">Warning</option>
+            <option value="danger">Danger</option>
+            <option value="info">Info</option>
+          </select>
+          <b className="text-left">Header Alert</b>
+          <input
+            type="text"
+            name="header"
+            className="form-control"
+            value={this.state.header}
+            onChange={this.changeHeaderAlert}
+          />
+          <b className="text-left">Content Alert</b>
+          <input
+            type="text"
+            name="content"
+            className="form-control"
+            value={this.state.content}
+            onChange={this.changeContentAlert}
+          />
+        </div>
+        <div></div>
       </>
     );
   }
@@ -40,22 +52,21 @@ class App extends Component {
     this.state = {
       type: "success",
       header: "React JS",
-      content: "Change Type Alert with React JS"
-    }
+      content: "Change Type Alert with React JS",
+    };
   }
-  // fungsi untuk mengubah tipe  
-changeTypeAlert = (event) =>{  
-  this.setState({type: event.target.value});  
-}  
-// fungsi untuk mengubah header  
-changeHeaderAlert = (event) => {  
-  this.setState({header: event.target.value});  
-}  
-// fungsi untuk mengubah content  
-changeContentAlert = (event) => {  
-  this.setState({content: event.target.value});  
-}  
-
+  // fungsi untuk mengubah tipe
+  changeTypeAlert = (event) => {
+    this.setState({ type: event.target.value });
+  };
+  // fungsi untuk mengubah header
+  changeHeaderAlert = (event) => {
+    this.setState({ header: event.target.value });
+  };
+  // fungsi untuk mengubah content
+  changeContentAlert = (event) => {
+    this.setState({ content: event.target.value });
+  };
 }
 
-export default App;  
+export default App;
